c43f2332b01df6f6ebabd2898e1b930d3e5b23a2
Alejandro Segovia <fcblade676@gmail.com>
2023-02-24 23:39:36 -0600

    1.3 Refactorizando los componentes, se cambio la manera de obtener los datos al ahora estar en objetos, se modificaron los componentes para que la aplicación siga funcionando

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -14,34 +14,44 @@ const Part = props => {
 const Content = props => {
   return(
     <div>
-      <Part part = {props.part1} exercise = {props.exercise1}/>
-      <Part part = {props.part2} exercise = {props.exercise2}/>
-      <Part part = {props.part3} exercise = {props.exercise3}/>
+      <Part part = {props.part1.name} exercise = {props.part1.exercises}/>
+      <Part part = {props.part2.name} exercise = {props.part2.exercises}/>
+      <Part part = {props.part3.name} exercise = {props.part3.exercises}/>
     </div>
   )
 }
   
-const Total = props => <p>Total: {props.value1 + props.value2 + props.value3}</p>
+const Total = props => 
+  <p>Total: 
+    {props.value1.exercises + props.value2.exercises + props.value3.exercises}
+  </p>
 
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+ const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course = {course}/>
 
       <Content 
-        part1 = {part1} exercise1 = {exercises1} 
-        part2 = {part2} exercise2 = {exercises2}
-        part3 = {part3} exercise3 = {exercises3}/>
+        part1 = {part1} exercise1 = {part1} 
+        part2 = {part2} exercise2 = {part2} 
+        part3 = {part3} exercise3 = {part3} />
       
-      <Total value1 = {exercises1} value2 = {exercises2} value3 = {exercises3}/>
+      <Total value1 = {part1} value2 = {part2} value3 = {part3}/>
+      
     </div>
   );
 };
