92679357ac0b627c6365695ba2e7bbb255a0ce2b
Alejandro Segovia <fcblade676@gmail.com>
2023-02-24 23:58:15 -0600

    1.4 Modificando las variables de App. Se colocaron los objetos en un array y se modificaron los demás componentes para que la aplicación siga funcionando

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -14,44 +14,44 @@ const Part = props => {
 const Content = props => {
   return(
     <div>
-      <Part part = {props.part1.name} exercise = {props.part1.exercises}/>
-      <Part part = {props.part2.name} exercise = {props.part2.exercises}/>
-      <Part part = {props.part3.name} exercise = {props.part3.exercises}/>
+      <Part part = {props.parts[0].name} exercise = {props.parts[0].exercises}/>
+      <Part part = {props.parts[1].name} exercise = {props.parts[1].exercises}/>
+      <Part part = {props.parts[2].name} exercise = {props.parts[2].exercises}/>
     </div>
   )
 }
   
 const Total = props => 
   <p>Total: 
-    {props.value1.exercises + props.value2.exercises + props.value3.exercises}
+    {props.value[0].exercises + props.value[1].exercises + props.value[2].exercises}
   </p>
 
 const App = () => {
  const course = 'Half Stack application development'
-  const part1 = {
+
+ const parts = [
+  {
     name: 'Fundamentals of React',
     exercises: 10
-  }
-  const part2 = {
+  },
+  {
     name: 'Using props to pass data',
     exercises: 7
-  }
-  const part3 = {
+  },
+  {
     name: 'State of a component',
     exercises: 14
   }
+]
 
   return (
     <div>
       <Header course = {course}/>
 
-      <Content 
-        part1 = {part1} exercise1 = {part1} 
-        part2 = {part2} exercise2 = {part2} 
-        part3 = {part3} exercise3 = {part3} />
-      
-      <Total value1 = {part1} value2 = {part2} value3 = {part3}/>
-      
+      <Content parts = {parts}/>
+
+      <Total value = {parts}/>
+   
     </div>
   );
 };
