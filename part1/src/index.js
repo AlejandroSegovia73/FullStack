d14bf409aadb166b0e7bc82c5798b0fd3159050d
Alejandro Segovia <fcblade676@gmail.com>
2023-02-26 00:56:08 -0600

    UseState

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,41 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-const Hello = ({name, age}) => {
+const Hello = ({ name, age }) => {
   const bornYear = () => {
-    const now = new Date()
+    const now = new Date();
     const yearNow = now.getFullYear();
-    return yearNow - age 
-  }
+    return yearNow - age;
+  };
 
   return (
     <div>
-      <h2>Hello {name}, u are {age} years old.</h2>
+      <h2>
+        Hello {name}, u are {age} years old.
+      </h2>
       <p>You were born in {bornYear()}</p>
     </div>
-  )
-}
+  );
+};
 
-const App = ({counter}) => {
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  setTimeout(() => {
+    setCounter(counter + 1);
+  }, 1000);
+
+  console.log("rendering...", counter);
+
   return (
     <div>
-      <Hello name = "Alejandro" age = {21}/>
-      <Hello name = "Firu" age = {2} />
+      <Hello name="Alejandro" age={21} />
+      <Hello name="Firu" age={2} />
       {counter}
     </div>
   );
 };
 
-let counter = 1;
-
-const refresh = () => {  
-  ReactDOM.render(<App 
-    counter={counter}/>,
-    document.getElementById("root")
-  );
-}
-setInterval(() => {
-  refresh();
-  counter += 1;
-
-}, 2000)
+ReactDOM.render(<App />, document.getElementById("root"));
