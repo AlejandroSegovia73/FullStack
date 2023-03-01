a6c69bca7db3afafeb624212f65ef0d20989219c
Alejandro Segovia <fcblade676@gmail.com>
2023-02-28 22:26:55 -0600

    Ambos estados left y right en un solo objeto

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,23 +1,47 @@
 import React, { useState } from "react";
-import ReactDOM from "react-dom";
+import {createRoot} from "react-dom/client";
 
 const App = () => {
   const [left, setLeft] = useState(0);
   const [right, setRight] = useState(0);
 
+  //Usando un objeto
+  const [clicks, setClicks] = useState({ left2: 0, right2: 0 });
+
+  const handleLeftClick = () => {
+    setClicks({
+      ...clicks,
+      left2: clicks.left2 + 1,
+    });
+  };
+  const handleRightClick = () => {
+    setClicks({
+      ...clicks,
+      right2: clicks.right2 + 1,
+    });
+  };
+
   return (
     <div>
-      {left}
-      <button onClick={() => setLeft(left + 1)}>
-          Left
-      </button>
-      
-      <button onClick={() => setRight(right + 1)}>
-          Right
-      </button>
-      {right}
+      <div>
+        {left}
+        <button onClick={() => setLeft(left + 1)}>Left</button>
+
+        <button onClick={() => setRight(right + 1)}>Right</button>
+        {right}
+      </div>
+
+      <div>
+        {clicks.left2}
+        <button onClick={handleLeftClick}>Left2</button>
+        <button onClick={handleRightClick}>Right2</button>
+        {clicks.right2}
+      </div>
     </div>
   );
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+//Nueva manera de renderizar desde react 18
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
