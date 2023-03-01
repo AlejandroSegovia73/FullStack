fd2036560c4b981d722b644c30cc587fa7014b75
Alejandro Segovia <fcblade676@gmail.com>
2023-02-28 20:34:23 -0600

    Estado complejo. 2 contadores individuales Left y Right

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,54 +1,21 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-const Hello = ({ name, age }) => {
-  const bornYear = () => {
-    const now = new Date();
-    const yearNow = now.getFullYear();
-    return yearNow - age;
-  };
-
-  return (
-    <div>
-      <h2>
-        Hello {name}, u are {age} years old.
-      </h2>
-      <p>You were born in {bornYear()}</p>
-    </div>
-  );
-};
-
-const Display = ({counter}) =>{
-  return(
-    <div>
-      {counter}
-    </div>
-  )
-}
-
-const Button = ({handleClick, text}) =>{
-  return(
-    <div>
-      <button onClick={handleClick}>
-        {text}
-      </button>
-    </div>
-  )
-}
-
 const App = () => {
-  const [counter, setCounter] = useState(0);
-  const plus = () => setCounter(counter + 1);
-  const reset = () => setCounter(0)
-  const minus = () => setCounter(counter - 1);
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+
   return (
     <div>
-      <Hello name="Alejandro" age={21} />
-      <Hello name="Firu" age={2} />
-      <Display counter = {counter}/>
-      <Button handleClick = {plus} text = '+'/>
-      <Button handleClick = {reset} text =' Reset'/>
-      <Button handleClick={minus} text = '-'/>
+      {left}
+      <button onClick={() => setLeft(left + 1)}>
+          Left
+      </button>
+      
+      <button onClick={() => setRight(right + 1)}>
+          Right
+      </button>
+      {right}
     </div>
   );
 };
